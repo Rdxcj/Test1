57d449be61dbdf4d6fef093f53df4855e9c31ed5
Rdxcj <167388492+Rdxcj@users.noreply.github.com>
2024-04-20 10:34:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ response = requests.post(
 )
 pr = json.loads(response.text)["streamingData"]["hlsManifestUrl"]
 print(pr)
-os.system(f"ffmpeg -ss 05:58:50 -re -i '{pr}' -max_muxing_queue_size 9999 -map 0:p:2 -c:v libx264 -c:a copy -g 30 -f flv rtmp://a.rtmp.youtube.com/live2/zvmf-1yjp-jzek-01pw-b4js")
+os.system(f"ffmpeg -ss 05:58:50 -re -i '{pr}' -max_muxing_queue_size 9999 -map 0:p:2 -c:v libx264 -c:a copy -g 30 -f flv rtmp://a.rtmp.youtube.com/live2/pv1s-w0jm-tjkh-zpds-4xuc")
